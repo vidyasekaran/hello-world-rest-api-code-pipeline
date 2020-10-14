daee0e835c22a678f137a2daea83f504239f37e6
unknown <vidguru1008@gmail.com>
2020-10-14 09:01:50 +0530

    yogananda commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -14,7 +14,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World - Changed - V2");
+		return new HelloWorldBean("Hello World - Changed - V3");
 	}
 	
 	@GetMapping(path = "/hello-world/path-variable/{name}")
